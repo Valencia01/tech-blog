acc05989ce7910e5e29e0dc1dbfea69126e82060
Valencia01 <kvalencia.99440@gmail.com>
2023-08-17 00:52:08 -0700

    added styling to both login and signup pages

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/homepage');
+    return;
+  }
+
+  res.render('homepage');
+});
+
+router.get('/homepage', withAuth, async (req, res) => {
+    try {
+        res.render('homepage');
+    }
+    catch (err) {
+        res.status
+    }
+    
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+module.exports = router;
